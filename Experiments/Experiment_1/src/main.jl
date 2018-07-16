57fd6ed441801527f51d9d4ed7ce0d3c42da47db
Agni <agni@MOHITs-MacBook-Pro.local>
2018-07-16 14:10:47 -0400

    Experiments Folder

---
+++ Experiments/Experiment_1/src/main.jl
@@ -0,0 +1,31 @@
+include("../src/evaluation_tools.jl")
+include("../src/evaluation_pipeline_exp.jl")
+
+datasets = readdir()
+# criter=[:silhouette,:dunnindex]
+criter=[:silhouette]
+clust_method=["localsearch","greedy"]
+
+datalistcriterionpairs =collect(Iterators.product(datasets,criter,clust_method))
+
+function main(;seed::Int64=2,
+               gridsearch::Bool=false,
+               num_tree_restarts::Int64=100,
+               complexity::Float64= 0.0,
+               minbucket::Int64=1,
+               datafolderpath::String="../data/",
+               resultsfolderpath::String="../results/")
+  arg_in = parse(Int64, ARGS[1])
+  (data, criterion, method) = datalistcriterionpairs[arg_in]
+  run_single(;data=data,
+             cr=criterion,
+             method = method,
+             seed=2,
+             gridsearch=false,
+             num_tree_restarts=100,
+             complexity= 0.0,
+             min_bucket=1,
+             maxdepth=3,
+             datafolderpath=datafolderpath,
+             resultsfolderpath=resultsfolderpath)
+end
---
+++ Experiments/Experiment_Analytics_Edge/src/evaluation_pipeline_exp.jl
@@ -0,0 +1,68 @@
+
+#Run the whole iteration for a single 
+function run_single(;data::String=data,
+             		 cr::Symbol=criterion,
+             		 method::String=method,
+            	     seed::Int64=2,
+             		 gridsearch::Bool=false,
+             		 num_tree_restarts::Int64=100,
+             		 complexity::Float64= 0.0,
+             		 min_bucket::Int64=1,
+             		 maxdepth::Int64=3,
+             		 datafolderpath::String=datafolderpath,
+             		 resultsfolderpath::String=resultsfolderpath)
+	#READ THE DATA
+	data = readtable(data); 
+	X = data[1:(end-1)]; 
+	y = data[:label]; 
+	truelabels = true; 
+	srand(seed)
+
+	#RUN OPTIMAL CLUSTERING TREES BOTH WITH GREEDY AND LOCAL SEARCH
+	if method =="localsearch"
+		lnr = OptimalTrees.OptimalTreeClassifier(ls_num_tree_restarts = num_tree_restarts, ls_random_seed = seed, cp = complexity, max_depth = maxdepth,
+		minbucket = min_bucket, criterion = cr, ls_warmstart_criterion = cr);
+		OptimalTrees.fit!(lnr, X, y);
+	elseif method =="greedy"
+		lnr = OptimalTrees.OptimalTreeClassifier(localsearch = false, cp = complexity, max_depth = maxdepth,
+		minbucket = min_bucket, criterion = cr);
+		OptimalTrees.fit!(lnr, X, y);
+	end
+	
+	#Get the statistics from the local search
+	leaf_cnt = leafcount(lnr) ## Defined in tools file
+	# optclust_depth = grid.best_params[:max_depth];
+	optclust_assignments = OptimalTrees.apply(lnr, X);
+	optclust_score = OptimalTrees.score(lnr, X, y, criterion = cr);
+
+	####### STEP 3: KMEANS. Run k means with the chosen depth, and in the neighborhood of the tree
+	# Determine range for k based on leaves of OptClust result
+	min_k = max(leaf_cnt - 2, 2); max_k = leaf_cnt + 2;
+	
+	# Return a dictionary of scores and assignments for each k, as well as the best k value (max score)
+	kmeans_scoredict, kmeans_assignmentdict, kmeans_best = eval_kmeans(X, min_k:max_k, s); ## Defined in tools file
+	ari_optclust_kmeans = randindex(optclust_assignments, kmeans_assignmentdict[leaf_cnt])[1];
+	
+	##### STEP 5: If ground truth available, compare
+	if truelabels
+		true_assignments = Array{Int64}(y)
+		true_k = length(unique(y));
+		true_silhouette = silhouette_score(X, true_assignments)
+		ari_true_kmeans = randindex(true_assignments, kmeans_assignmentdict[true_k])[1]
+		if true_k == leaf_cnt 
+			ari_true_optclust = randindex(true_assignments, optclust_assignments)[1]
+		else ari_true_optclust = -10
+		end
+	else true_assignments = -10; true_k = -10; true_silhouette = -10; ari_true_kmeans = -10;
+	end
+	#Results
+	### Save results in an array to paste into Excel file 
+	results = DataFrame(seed = s, K_optclust=leaf_cnt, kmeans_k=kmeans_best, true_k=true_k ,
+	score_optclust = optclust_score , score_kmeans_bestk=kmeans_scoredict[kmeans_best],  score_kmeans_kc=kmeans_scoredict[leaf_cnt], score_true=true_silhouette,
+	ari_optclust_kmeans=ari_optclust_kmeans, ari_true_kmeans=ari_true_kmeans, are_true_optclust=ari_true_optclust)
+
+	filepath_lnr = joinpath(resultsfolderpath, "lnr$data-$criterion-$method-lnr.jld")
+    @save filepath_lnr lnr
+    filepath_accuracy = joinpath(resultsfolderpath, "results$data-$criterion-$method.csv")
+  	writetable(filepath_accuracy, results)
+end
