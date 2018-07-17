4a1c73c16749af6c84d9ffd343b55fd63778e7c0
hwiberg718 <hmw54@cornell.edu>
2018-07-17 16:26:22 -0400

    Remove duplicate experiment folder from analytics edge

--- Experiments/Experiment_Analytics_Edge/src/run-main.jl
+++
@@ -1,8 +0,0 @@
-include("../src/main.jl")
-main(;seed=2,
-      gridsearch=false,
-      num_tree_restarts=100,
-      complexity= 0.0,
-      minbucket=1,
-      datafolderpath="../data/",
-      resultsfolderpath="../results/")
