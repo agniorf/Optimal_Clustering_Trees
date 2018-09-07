54d5e2d0d446380fa21fad54db7e65558a27da14
hwiberg718 <hmw54@cornell.edu>
2018-09-07 17:33:33 -0400

    Update batch script for robust dunn tests

--- Experiments/Experiment_1/src/evaluation_tools.jl
+++ Experiments/Experiment_1/src/evaluation_tools.jl
@@ -116,12 +116,68 @@ function dunn_score(lnr, assignments)
 
 end
 
+function robustdunn_score(lnr, assignments)
+
+	K = length(unique(assignments));
+	distance_matrix = lnr.prb_.data.features.distance_matrix;
+
+	assign_df = DataFrame(hcat(collect(1:size(assignments,1)), assignments));
+
+	n = size(assign_df,1)
+	pairwise_n = n*(n-1)/2
+
+	if K == 1
+		score = -10000000
+	else
+	# Find top λ% of diameters and sum them
+	clust_dict = Dict{Int64, Array{Int64,1}}()
+
+	intraclust_distances = []
+	maximum_diameter = 0
+	intra_n = 0
+	for clust in unique(assignments)
+		# println("Cluster number = ", clust)
+		index_list = assign_df[assign_df[:x2] .== clust, :x1];
+		clust_dict[clust] = index_list;
+		cluster_distances = distance_matrix[index_list, index_list];
+		intraclust_distances = vcat(intraclust_distances, collect(Iterators.flatten(cluster_distances)));
+		intra_n += size(index_list,1)*(size(index_list,1)-1)/2
+		# println("Intra_n = $(intra_n); elements = $(size(index_list,1))")
+	end
+
+	intra_cutoff = convert(Int64, round(λ*intra_n/2));
+	sorted_intraclust = sort(unique(intraclust_distances), rev = true);
+	sum_high_diams = sum(sorted_intraclust[1:intra_cutoff]);
+
+	betweenclust_distances = []
+	between_n = pairwise_n - intra_n
+	minimum_separation = 1000000
+	for c1 in unique(assignments), c2 in setdiff(unique(assignments),c1)
+	  	if c1 < c2
+	  		cluster_distances = distance_matrix[clust_dict[c1], clust_dict[c2]]
+	    	betweenclust_distances = vcat(betweenclust_distances, collect(Iterators.flatten(cluster_distances)));
+	  	end
+	end
+
+	between_cutoff = convert(Int64, round(λ*between_n/2));
+	sorted_betweenclust = sort(unique(betweenclust_distances));
+	sum_low_seps = sum(sorted_betweenclust[1:between_cutoff]);
+
+	score = (sum_low_seps/between_cutoff)/(sum_high_diams/intra_cutoff)
+	end
+
+	return score
+end
+
+
 
 function cluster_score(lnr, assignments, cr)
 	if cr == :silhouette
 		score = silhouette_score(lnr, assignments)
 	elseif cr == :dunnindex
 		score = dunn_score(lnr, assignments)
+	elseif cr = :robustdunn
+		score = robustdunn_score(lnr, assignments)
 	else score = -10
 	end
 	return score
--- Julia_code/ICO_test.jl
+++ Julia_code/ICO_test.jl
@@ -3,11 +3,12 @@ using Clustering, Distances
 using RDatasets
 using OptimalTrees
 
-dataset = readtable("../data/localSearch_7.csv"); 
+# dataset = readtable("../data/localSearch_7.csv"); 
+dataset = readtable("../Testing_Class_Project/data/TwoDiamonds.csv")
 X = dataset[1:end-1]; y = dataset[end];
 
 s = 2;
-cr = :silhouette; 
+cr = :robustdunn; 
 
 
 lnr_greedy = OptimalTrees.OptimalTreeClassifier(localsearch = false, cp = 0.0,
@@ -18,6 +19,6 @@ OptimalTrees.showinbrowser(lnr_greedy)
 
 lnr_local = OptimalTrees.OptimalTreeClassifier(ls_num_tree_restarts = 5, cp = 0.0, ls_random_seed = s,
 	max_depth = 3, minbucket = 1, criterion = cr, show_progress_bar=true, ls_warmstart_criterion= cr);
-OptimalTrees.fit!(lnr_local, X, y);
+@time OptimalTrees.fit!(lnr_local, X, y);
 b = OptimalTrees.score(lnr_local, X, y);
 OptimalTrees.showinbrowser(lnr_local)
