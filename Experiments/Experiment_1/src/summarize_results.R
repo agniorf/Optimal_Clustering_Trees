7ca11a43e11f6fac7bd15de90f145456ec114e14
hwiberg718 <hmw54@cornell.edu>
2018-09-11 15:49:09 -0400

    reorganize robust dunn results

--- Experiments/Experiment_1/src/summarize_results.R
+++ Experiments/Experiment_1/src/summarize_results.R
@@ -2,21 +2,22 @@ library(data.table)
 library(tidyverse)
 
 setwd("~/Packages/Optimal_Clustering_Trees/Experiments/Experiment_1/results")
+# setwd("~/git/Optimal_Clustering_Trees/Experiments/Experiment_1/results")
 temp = list.files(pattern="*.csv")
 temp_csv<-temp[which(!temp %like% "jld")]
 
 myfiles = lapply(temp_csv, read.delim)
 
-datasetlist <- c("Atom","Hepta","Lsun","Target",
-                    "Tetra","TwoDiamonds","WingNut") 
-metriclist <- c("silhouette","dunnindex")
-methodlist <- c("localsearch","greedy")
+datasetlist <- c("Hepta","Lsun","Target",
+                    "Tetra","TwoDiamonds","Atom","WingNut") 
+metriclist <- c("silhouette","dunnindex","robustdunn")
+methodlist <- c("greedy","localsearch")
 
 df <- data.frame()
 df_params <- data.frame()
 
-for (dataname in datasetlist) {
-  for (metric in metriclist) {
+for (metric in metriclist) {
+  for (dataname in datasetlist) {
     for (method in methodlist) {
     filename <- paste0("results-",
                        dataname,".csv-",metric,"-",method,".csv")
