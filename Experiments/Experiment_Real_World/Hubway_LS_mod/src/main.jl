476a5dae3a6d6379bd4264b375f8315d78c96277
hwiberg718 <hmw54@cornell.edu>
2018-10-19 15:51:51 -0400

    reduce to single batch experiment

--- Experiments/Experiment_Real_World/Hubway_LS_mod/src/main.jl
+++ Experiments/Experiment_Real_World/Hubway_LS_mod/src/main.jl
@@ -1,14 +1,15 @@
 include("../src/evaluation_tools.jl")
 include("../src/evaluation_pipeline_exp.jl")
 
-datasets = readdir("../data/")
-criter=[:silhouette,:dunnindex,:robustdunn]
-clust_method=["localsearch"]
-thresholds = [.75,.9]
+# datasets = readdir("../data/")
+# criter=[:silhouette,:dunnindex,:robustdunn]
+# clust_method=["localsearch"]
+# thresholds = [.75,.9]
 
-datalistcriterionpairs_withgeom =collect(Iterators.product(datasets,criter,clust_method,true,thresholds));
-datalistcriterionpairs_nogeom =collect(Iterators.product(datasets,criter,clust_method,false,0.0));
-datalistcriterionpairs = cat(4, datalistcriterionpairs_withgeom, datalistcriterionpairs_nogeom);
+
+# # datalistcriterionpairs_withgeom =collect(Iterators.product(datasets,criter,clust_method,true,thresholds));
+# # datalistcriterionpairs_nogeom =collect(Iterators.product(datasets,criter,clust_method,false,0.0));
+# # datalistcriterionpairs = cat(4, datalistcriterionpairs_withgeom, datalistcriterionpairs_nogeom);
 
 function main(;seed::Int64=2,
                gridsearch::Bool=false,
@@ -18,7 +19,9 @@ function main(;seed::Int64=2,
                datafolderpath::String="../data/",
                resultsfolderpath::String="../results/")
   arg_in = parse(Int64, ARGS[1])
-  (data, criterion, method, geom_bool, gt_threshold) = datalistcriterionpairs[arg_in]
+  # (data, criterion, method, geom_bool, gt_threshold) = datalistcriterionpairs[arg_in]
+  data = "random_hubway_sample_seed1_obs_5000.csv"; criter = :silhouette; method = "localsearch";
+  geom_bool = true;  theshold = .9; 
   run_single(;data=data,
              cr=criterion,
              method = method,
