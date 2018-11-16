93180ac87e663b0e1ea214e300d369f1c6d08c0a
hwiberg718 <hmw54@cornell.edu>
2018-11-15 21:07:26 -0500

    set up test script for warmstart

--- Julia_code/ICO_warmstart.jl
+++ Julia_code/ICO_warmstart.jl
@@ -14,11 +14,19 @@ using BenchmarkTools
 # X = dataset[1:end-1]; y = dataset[end];
 
 data = dataset("cluster", "ruspini");
-data_array = convert(Array{Float64}, data);
-data_t = data_array';
+data = readtable("../data/fscs/Lsun.csv"); 
 
 K = 3;
-srand(1234);
+
+s = 2;
+cr = :silhouette; 
+bucket = 1; depth = 3; restarts = 10;
+
+data_array = convert(Array{Float64}, data);
+n, p = size(data_array)
+data_t = data_array';
+
+srand(s);
 kmeans_result = kmeans(data_t, K);
 
 assignments = kmeans_result.assignments;
@@ -26,29 +34,41 @@ data_full = DataFrame(hcat(data, assignments));
 rename!(data_full, :x1, :kmean_assign);
 # plot(dataset_full, x = :V2, y = :V3, color = :kmean_assign)
 
-X = data_full[1:2]; y = data_full[end];
+X = data_full[1:p]; y = data_full[end];
 
-s = 2;
-cr = :silhouette; 
 
 ## RUN optimal trees independently
-lnr_oct = OptimalTrees.OptimalTreeClassifier(localsearch = false,
-	max_depth = 3, minbucket = 1, criterion = :misclassification);
-@time OptimalTrees.fit!(lnr_oct, X, y);
-OptimalTrees.showinbrowser(lnr_oct);
+# lnr_oct = OptimalTrees.OptimalTreeClassifier(localsearch = false,
+# 	max_depth = 3, minbucket = 1, criterion = :misclassification);
+# @time OptimalTrees.fit!(lnr_oct, X, y);
+# OptimalTrees.showinbrowser(lnr_oct);
 
 # reload("OptimalTrees")
 lnr_greedy = ICOT.OptimalTreeClassifier(localsearch = false, cp = 0.0,
-	max_depth = 3, minbucket = 1, criterion = cr, ls_warmstart_criterion = cr);
-@time ICOT.fit!(lnr_greedy, X, y);
-a = ICOT.score(lnr_greedy, X, y);
+	max_depth = depth, minbucket = bucket, criterion = cr, ls_warmstart_criterion = cr,
+	kmeans_warmstart = false);
+@btime ICOT.fit!(lnr_greedy, X, y);
+score_greedy = ICOT.score(lnr_greedy, X, y);
 
 ICOT.showinbrowser(lnr_greedy)
 
-reload("OptimalTrees")
-lnr_local = ICOT.OptimalTreeClassifier(ls_num_tree_restarts = 10, cp = 0.0, ls_random_seed = s,
-	max_depth = 3, minbucket = 2, criterion = cr, show_progress_bar=true, ls_warmstart_criterion= cr, geom_search=true, geom_threshold=0.9);
+
+lnr_local = ICOT.OptimalTreeClassifier(ls_num_tree_restarts = restarts, cp = 0.0, ls_random_seed = s,
+	max_depth = depth, minbucket = bucket, criterion = cr, show_progress_bar=true, 
+	ls_warmstart_criterion= cr, geom_search=true, geom_threshold=0.9, 
+	kmeans_warmstart = false);
 @btime ICOT.fit!(lnr_local, X, y);
-b = ICOT.score(lnr_local, X, y);
+score_local = ICOT.score(lnr_local, X, y);
 
 ICOT.showinbrowser(lnr_local)
+
+
+lnr_ws = ICOT.OptimalTreeClassifier(ls_num_tree_restarts = restarts, cp = 0.0, ls_random_seed = s,
+	max_depth = depth, minbucket = bucket, criterion = cr, show_progress_bar=true, 
+	ls_warmstart_criterion= cr, geom_search=true, geom_threshold=0.9,
+	kmeans_warmstart = true);
+@btime ICOT.fit!(lnr_ws, X, y);
+score_ws = ICOT.score(lnr_ws, X, y);
+
+ICOT.showinbrowser(lnr_ws)
+
