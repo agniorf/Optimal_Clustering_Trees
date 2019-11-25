f7b61236bb048ac44d3be4ef73e80997293b06a2
Holly Wiberg <hollywiberg@hollys-mbp.lan>
2019-11-25 18:20:17 -0500

    add sh script for comparison

---
+++ revision/complete_comparison_fcps/sh/run-main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --array=1-18
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=16000
+#SBATCH -p sched_mit_sloan_batch
+#SBATCH --time=2-00:00
+#
+export IAI_LICENSE_FILE="$HOME/iai-licenses/${SLURMD_NODENAME}.lic"
+srun julia -J ~/software/julia-1.1.0/lib/julia/sys.so ../src/run-main.jl $SLURM_ARRAY_TASK_ID
