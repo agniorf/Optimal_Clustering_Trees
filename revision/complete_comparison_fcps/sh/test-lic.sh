e764291a6bf577d995dddec94bb349a2f2ea08d0
hwiberg <hwiberg@eosloan.eth.cluster>
2019-11-29 14:50:10 -0500

    add EngyTime results across methods

--- revision/complete_comparison_fcps/sh/test-lic.sh
+++ revision/complete_comparison_fcps/sh/test-lic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array=86
+#SBATCH --array=6-90
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16000
 #SBATCH -p sched_mit_sloan_batch
 #SBATCH --time=2-00:00
 #
-export IAI_LICENSE_FILE="$HOME/iai.lic"
+export IAI_LICENSE_FILE="$HOME/iai-licenses/${SLURMD_NODENAME}.lic"
 srun julia -J ~/software/julia-1.1.0/lib/julia/sys.so ../src/run_compare.jl $SLURM_ARRAY_TASK_ID
