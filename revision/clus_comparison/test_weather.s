3bab62c393530ab7de8b7ac6667b382013768066
Holly Wiberg <hollywiberg@dhcp-18-21-130-6.dyn.mit.edu>
2019-12-02 13:46:47 -0500

    add clus comparison example

---
+++ revision/clus_comparison/test_weather.s
@@ -0,0 +1,10 @@
+[General]
+RandomSeed = 1
+
+[Attributes]
+Target = 1-4 ## range of target attributes.
+Clustering = 1-4 ## which features to cluster baseed on
+Descriptive = 1-4 ## features used in descriptive part of model (default = all non-target)
+
+[Tree]
+Heuristic = VarianceReduction
