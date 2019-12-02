d4d2bd10776f984c7243158297fb1fbe389d8c9c
Holly Wiberg <hollywiberg@dhcp-18-21-130-6.dyn.mit.edu>
2019-12-02 13:51:08 -0500

    clus experiment setup

--- revision/clus_comparison/test_weather.s
+++ revision/clus_comparison/test_weather.s
@@ -2,9 +2,9 @@
 RandomSeed = 1
 
 [Attributes]
-Target = 1-4 ## range of target attributes.
-Clustering = 1-4 ## which features to cluster baseed on
-Descriptive = 1-4 ## features used in descriptive part of model (default = all non-target)
+Target = 1-4
+Clustering = 1-4 
+Descriptive = 1-4 
 
 [Tree]
 Heuristic = VarianceReduction
