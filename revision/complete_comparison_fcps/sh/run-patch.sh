9a231e3e9b0a5a81888e6e42da317893c54cc50b
hwiberg <hwiberg@eosloan.eth.cluster>
2019-12-06 16:57:42 -0500

    add patched scaling results

--- revision/complete_comparison_fcps/sh/run-patch.sh
+++ revision/complete_comparison_fcps/sh/run-patch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --array=1-180
+#SBATCH --array=11-89
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16000
 #SBATCH -p sched_mit_sloan_batch
 #SBATCH --time=2-00:00
 #
 export IAI_LICENSE_FILE="$HOME/iai.lic"
-srun julia -J ~/software/julia-1.1.0/lib/julia/sys.so ../src/run_compare.jl $SLURM_ARRAY_TASK_ID
+srun julia -J ~/software/julia-1.1.0/lib/julia/sys.so ../src/run_compare_patch.jl $SLURM_ARRAY_TASK_ID
