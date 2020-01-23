af02b4eddd270532579cce1e1db13186a6a68a5f
Holly Wiberg <hollywiberg@Hollys-MBP.cable.rcn.com>
2020-01-23 11:40:40 -0500

    fhs experiments

---
+++ revision/clus_comparison/experiments_FHS/FHS_depth2_seed1.s
@@ -0,0 +1,21 @@
+[Data]
+                                PruneSet = None
+                                TestSet = None
+                                
+                                [General]
+                                RandomSeed = 1
+                                
+                                [Constraints]
+                                MaxDepth = 2
+                                
+                                [Attributes]
+                                Target = 1-21
+                                Clustering = 1-21 
+                                Descriptive = 1-21 
+                                
+                                [Tree]
+                                Heuristic = VarianceReduction
+                                
+                                [Output]
+                                WritePredictions = {Train}
+                                
